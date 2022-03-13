14f62119e99bea337b97e021faabed93a57bf74a
User <zh.grebyonkin@yandex.ru>
2022-03-13 15:21:03 +0500

    month_2

--- lessons/lesson_003/01_days_in_month.py
+++ lessons/lesson_003/01_days_in_month.py
@@ -10,5 +10,30 @@
 user_input = input("Введите, пожалуйста, номер месяца: ")
 month = int(user_input)
 print('Вы ввели', month)
-
-# TODO здесь ваш код
+if 0 < month < 13:
+    if month == 1:
+        print(31, 'день')
+    if month == 2:
+        print(28, 'деней')
+    if month == 3:
+        print(31, 'день')
+    if month == 4:
+        print(30, 'деней')
+    if month == 5:
+        print(31, 'день')
+    if month == 6:
+        print(30, 'деней')
+    if month == 7:
+        print(31, 'день')
+    if month == 8:
+        print(31, 'день')
+    if month == 9:
+        print(30, 'день')
+    if month == 10:
+        print(31, 'день')
+    if month == 11:
+        print(30, 'деней')
+    if month == 12:
+        print(31, 'день')
+else:
+    print('Введите корректный месяц')
