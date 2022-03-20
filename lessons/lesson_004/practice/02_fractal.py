c707758b598579d4e2c2f34dbcd3cd8905b894ec
User <zh.grebyonkin@yandex.ru>
2022-03-20 13:23:21 +0500

    fractal

--- lessons/lesson_004/practice/02_fractal.py
+++ lessons/lesson_004/practice/02_fractal.py
@@ -2,20 +2,43 @@
 
 import simple_draw as sd
 
-sd.resolution = (600, 800)
+sd.resolution = (600, 600)
 
 # нарисовать ветку дерева из точки (300, 5) вертикально вверх длиной 100
 
-point_0 = sd.get_point(300, 5)
+point = sd.get_point(x=300, y=5)
+v1 = sd.get_vector(start_point=point, angle=90, length=100, width=3)
+# v1.draw()
+# point_0 =  sd.get_point(300, 5)
 
 
 # сделать функцию рисования ветки из заданной точки,
 # заданной длины, с заданным наклоном
+point_0 = sd.get_point(100, 50)
+
+
+def branch(point, angle, length, width=3):
+    v1 = sd.get_vector(start_point=point, angle=angle, length=length, width=width)
+    v1.draw()
+    return v1.end_point
+
+
+# angle_0 =90
+# length_0 =200
+# next_point = branch(point=point_0, angle=angle_0, length=length_0)
+# next_angle = angle_0-30
+# next_length = length_0*0.75
+# next_point = branch(point=next_point, angle=next_angle, length=next_length)
+# next_angle = next_angle-30
+# next_length = next_length*0.75
+# next_point = branch(point=next_point, angle=next_angle, length=next_length)
+
+#
 # def branch(point, angle, length):
 #     v1 = sd.get_vector(start_point=point, angle=angle, length=length, width=3)
 #     v1.draw()
 #     return v1.end_point
-
+#
 # angle_0 = 90
 # length_0 = 200
 # next_point = branch(point=point_0, angle=angle_0, length=length_0)
@@ -35,28 +58,47 @@ point_0 = sd.get_point(300, 5)
 # next_point = point_0
 #
 # while next_length > 1:
+#     next_angle =next_angle-30
+#     next_length=next_length*0.75
+#     next_point=branch(point=next_point,angle=next_angle,length=next_length)
+
+def branch(point, angle, length, width=3):
+    if length < 1:
+        return
+    v1 = sd.get_vector(start_point=point, angle=angle, length=length, width=width)
+    v1.draw()
+    next_point = v1.end_point
+    next_angle = angle - 30
+    next_length = length * 0.75
+    branch(point=next_point, angle=next_angle, length=next_length)
+
+
+for length in range(50, 251, 50):
+    branch(point=point_0, angle=90, length=length)
+
+#
+# while next_length > 1:
 #     next_point = branch(point=next_point, angle=next_angle, length=next_length)
 #     next_angle = next_angle - 30
 #     next_length = next_length * .75
 
 # сделать функцию branch рекурсивной
 
-def branch(point, angle, length, delta):
-    if length < 1:
-        return
-    v1 = sd.get_vector(start_point=point, angle=angle, length=length, width=3)
-    v1.draw()
-    next_point = v1.end_point
-    next_angle = angle - delta
-    next_length = length * .75
-    branch(point=next_point, angle=next_angle, length=next_length, delta=delta)
-
-
-for delta in range(0, 51, 10):
-    branch(point=point_0, angle=90, length=150, delta=delta)
-for delta in range(-50, 1, 10):
-    branch(point=point_0, angle=90, length=150, delta=delta)
+# def branch(point, angle, length, delta):
+#     if length < 1:
+#         return
+#     v1 = sd.get_vector(start_point=point, angle=angle, length=length, width=3)
+#     v1.draw()
+#     next_point = v1.end_point
+#     next_angle = angle - delta
+#     next_length = length * .75
+#     branch(point=next_point, angle=next_angle, length=next_length, delta=delta)
+#
+#
+# for delta in range(0, 51, 10):
+#     branch(point=point_0, angle=90, length=150, delta=delta)
+# for delta in range(-50, 1, 10):
+#     branch(point=point_0, angle=90, length=150, delta=delta)
 
 
 sd.pause()
-
